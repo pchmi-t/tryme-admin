0c95e014a93f52301c65d6214d3d38d122e267e0
pchmi-t <vincitquisevincit@abv.bg>
2017-01-07 21:55:04 +0200

    Configura Heroku run & ElephantSQL US DB instance

--- trymeadmin/urls.py
+++ trymeadmin/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
 from jsonrpc import jsonrpc_site
 from trymeadmin import views
 

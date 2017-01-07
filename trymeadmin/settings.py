36d0c54472bbb5a5233ad298855df56c6d6ea7f5
pchmi-t <vincitquisevincit@abv.bg>
2017-01-07 21:40:12 +0200

    Configura Heroku run & ElephantSQL US DB instance

--- trymeadmin/settings.py
+++ trymeadmin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,8 @@ WSGI_APPLICATION = 'trymeadmin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+database_url = os.environ['DATABASE_URL']
+DATABASES['default'] = dj_database_url.parse(database_url, conn_max_age=600)
 
 
 # Password validation
